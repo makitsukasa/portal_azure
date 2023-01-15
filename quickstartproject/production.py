9b25c67f3be67db67d537290640ce6e2542ddb43
makitsukasa <allium.fistulosum.39@gmail.com>
2023-01-15 23:06:24 +0900

    clean up

--- quickstartproject/production.py
+++ quickstartproject/production.py
@@ -6,17 +6,17 @@ import os
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
 
 # WhiteNoise configuration
-MIDDLEWARE = [                                                                   
-    'django.middleware.security.SecurityMiddleware',
-# Add whitenoise middleware after the security middleware                             
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',                      
-    'django.middleware.common.CommonMiddleware',                                 
-    'django.middleware.csrf.CsrfViewMiddleware',                                 
-    'django.contrib.auth.middleware.AuthenticationMiddleware',                   
-    'django.contrib.messages.middleware.MessageMiddleware',                      
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',                    
+MIDDLEWARE = [
+	'django.middleware.security.SecurityMiddleware',
+# Add whitenoise middleware after the security middleware
+	'whitenoise.middleware.WhiteNoiseMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
