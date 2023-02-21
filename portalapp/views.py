e8ea7b8950beefc3b65766bf585cba3d7c1cc940
makitsukasa <allium.fistulosum.39@gmail.com>
2023-02-21 22:39:47 +0900

    bookmark

--- portalapp/views.py
+++ portalapp/views.py
@@ -1,4 +1,22 @@
+import os
+import json
 from django.shortcuts import render
 
 def index(request):
-	return render(request, 'portal.html', context={})
+	bookmarks = os.getenv("CUSTOMCONNSTR_BOOKMARKS", False)
+	if bookmarks:
+		context = {"bookmarks": json.loads(bookmarks)}
+	else:
+		context = {"bookmarks": [
+			{"title": "google", "url": "https://google.com", "show_url": "google.com"},
+			{"title": "hidden", "url": "/calendar/2", "show_url": "/calendar/2"},
+		]}
+
+	for i in range(len(context["bookmarks"])):
+		if context["bookmarks"][i]["url"].startswith("/"):
+			context["bookmarks"][i]["url"] = request._current_scheme_host + context["bookmarks"][i]["url"]
+
+	return render(
+		request = request,
+		template_name = 'portal.html',
+		context = context)
